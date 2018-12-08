e66cbd730e39473c9383892e27e19dacdd320bc6
Surbhi <surbhi910@gmail.com>
2018-12-08 02:15:05 -0500

    Missing functionality for likeModel implemented

--- data/dao/like.dao.server.js
+++ data/dao/like.dao.server.js
@@ -3,9 +3,12 @@ const likeModel = require('../models/like/like.model.server');
 
 function findAllFavouriteMovies(){
 
+    return likeModel.find().populate('Movie user')
+
 }
 
 function deleteFavouriteMovie(){
+    return likeModel.remove({_id: id})
 
 }
 
