15e28efb19c7f0bae2bfef111f20c56fe1ca60e7
Surbhi <surbhi910@gmail.com>
2018-12-10 12:18:29 -0500

    Reverting client url back to 3000 port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 const client_heroku_url = "http://desolate-retreat-56126.herokuapp.com";
-const client_local_url = "*";
+const client_local_url = "http://localhost:3000";
 
 
 app.use(function (req, res, next) {
